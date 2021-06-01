da328c751b3f6454fccf7d3d336ca99cdd2a1fa7
Sébastien de Melo <sebastien@singlespot.com>
2021-06-01 18:48:04 +0200

    prod

--- scripts/configure-vault-eks.sh
+++ scripts/configure-vault-eks.sh
@@ -4,8 +4,8 @@ kubectl apply -f vault-auth.yaml
 kubectl apply -f vault-auth-services.yaml
 
 export VAULT_ADDR=https://vault.singlespot.com
-export K8S_HOST=https://7C69069F587A4E20F7F54E88F95D4ED0.sk1.eu-west-1.eks.amazonaws.com
-K8S_CLUSTER=eks-preprod
+export K8S_HOST=https://F7D00A91EA64EBC28385ABE542A6E0CC.gr7.eu-west-1.eks.amazonaws.com
+K8S_CLUSTER=eks-prod
 VAULT_SA_NAME=vault-auth
 export VAULT_SA_TOKEN_NAME=$(kubectl -n default get sa $VAULT_SA_NAME -o jsonpath="{.secrets[0]['name']}")
 export SA_CA_CRT=$(kubectl -n default get secret $VAULT_SA_TOKEN_NAME -o jsonpath="{.data['ca\.crt']}" | base64 --decode; echo)
